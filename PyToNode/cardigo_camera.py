5b8e369af5bd82077746393ec1dc33b74adc9457
amgugu111 <amgugu111@gmail.com>
2020-06-09 01:06:27 +0530

    fixed GUI

--- PyToNode/cardigo_camera.py
+++ PyToNode/cardigo_camera.py
@@ -8,6 +8,7 @@ from PIL import Image, ImageTk
 from urllib.request import urlopen
 from io import BytesIO
 import base64
+import json
 
 def selectPath():
 	dir_name = filedialog.askdirectory(title='Select Path')
@@ -33,10 +34,14 @@ def runApp():
 		cv2.imwrite(final_path,frame)
 		image_file_descriptor = open(final_path, 'rb')
 		files = {'image/jpeg': image_file_descriptor}
-		url = 'https://node-red-vlxgg.eu-gb.mybluemix.net/mns'
-		r=requests.post(url, files=files)
-		print(r.status_code)
+		imgUrl = 'https://node-red-vlxgg.eu-gb.mybluemix.net/mns'
+		r=requests.post(imgUrl, files=files)
+		print('Photo Status Code'+str(r.status_code))
 		image_file_descriptor.close()
+		delIdUrl = 'https://node-red-vlxgg.eu-gb.mybluemix.net/Id'
+		empId = {'Id':delId}
+		rId=requests.post(delIdUrl,json=empId)
+		print('ID status Code'+str(rId.status_code))
 		i+=1
 		print(i)
 		videoCaptureObject.release()
